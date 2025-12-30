002ccd0a7c8c22b5b14d2a17b6fdbf9e561ea6e1
molly venegas <mollyvenegasvasquez@gmail.com>
2025-12-30 20:19:02 -0300

    se mejoro el codigo del catalogo

--- src/componets/product_catalog/Product_catalog.tsx
+++ src/componets/product_catalog/Product_catalog.tsx
@@ -1,45 +1,21 @@
 import "./Product_catalog.css"
-import alfajores from "../../assets/alfajores.jpeg";
-import cachitos from "../../assets/cachitos.jpeg";
-import chilenitos from "../../assets/chilenitos.jpeg";
-import mendocinosCocktail from "../../assets/mendocinos-cocktail.jpeg";
-import mendocinosGrandes from "../../assets/mendocinos-grandes.jpeg";
+import {products} from "./../../data/products"
 
 export function Product_catalog() {
     return (
         <div className="product_catalog">
             <h1>Productos</h1>
-            <div className="product_grid"> 
-                <div className="product">
-                    <img src={alfajores} alt="Alfajores"/>
-                    <h2>alfajores</h2>
-                    <h3>caja de 6 unidades</h3>
-                    <p>$3.000</p>
-                </div>
-                    <div className="product">
-                    <img src={cachitos} alt="Cachitos"/>
-                    <h2>cachitos</h2>
-                    <h3>caja de 8 unidades</h3>
-                    <p>$3.000</p>
-                </div>                
-                    <div className="product">
-                    <img src={chilenitos} alt="Chilenitos"/>
-                    <h2>chilenitos</h2>
-                    <h3>caja de 4 unidades</h3>
-                    <p>$3.000</p>
-                </div>                
-                    <div className="product">
-                    <img src={mendocinosCocktail} alt="Mendocinos cocktail"/>
-                    <h2>mendocino cocktail</h2>
-                    <h3>caja de 8 unidades</h3>
-                    <p>$3.000</p>
-                </div>                
-                    <div className="product">
-                    <img src={mendocinosGrandes} alt="Mendocinos grandes"/>
-                    <h2>mendocinos grandes</h2>
-                    <h3>caja de 4 unidades</h3>
-                    <p>$3.000</p>
-                </div>
+            <div className="product_grid">
+                {products.map((product)=>{
+                    return(
+                    <div className="product" key={product.id}>
+                        <img src={product.image} alt={product.alt}/>
+                        <h2>{product.name}</h2>
+                        <h3>{product.description}</h3>
+                        <p>${product.price}</p>
+                    </div>
+                    )
+                })}
             </div>
         </div>
     )
---
+++ src/data/products.ts
@@ -0,0 +1,48 @@
+import alfajores from "../assets/alfajores.jpeg";
+import cachitos from "../assets/cachitos.jpeg";
+import chilenitos from "../assets/chilenitos.jpeg";
+import mendocinosCocktail from "../assets/mendocinos-cocktail.jpeg";
+import mendocinosGrandes from "../assets/mendocinos-grandes.jpeg";
+
+export const products =[
+    {
+        id:"alfajores",
+        name:"Alfajores",
+        description:"caja de 6 unidades",
+        price:3000,
+        image:alfajores,
+        alt:"alfajores"
+    },
+    {
+        id:"cachitos",
+        name:"Cachitos",
+        description:"caja de 8 unidades",
+        price:3000,
+        image:cachitos,
+        alt:"cachitos"
+    },
+    {
+        id:"chilenitos",
+        name:"Chilenitos",
+        description:"caja de 4 unidades",
+        price:3000,
+        image:chilenitos,
+        alt:"chilenitos"
+    },
+    {
+        id:"mendocinos-cocktail",
+        name:"Mendocinos-cocktail",
+        description:"caja de 8 unidades",
+        price:3000,
+        image:mendocinosCocktail,
+        alt:"mendocinos-cocktail"
+    },
+    {
+        id:"mendocinos-grandes",
+        name:"Mendocinos-grandes",
+        description:"caja de 4 unidades",
+        price:3000,
+        image:mendocinosGrandes,
+        alt:"mendocinos-grandes"
+    },
+];
